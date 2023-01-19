b8662cf1ea0b743214ad61b7adcf0393182c550f
Dexametasona <machicadogomezalexander@gmail.com>
2023-01-19 09:50:50 -0500

    push para el profesor

--- src/app/guards/permiso-salir.guard.ts
+++ src/app/guards/permiso-salir.guard.ts
@@ -9,10 +9,26 @@ import Swal from 'sweetalert2';
 /* guard para el form en caso que el usuario que ha modificado el form */
 export class PermisoSalirGuard implements CanDeactivate<boolean>{
   canDeactivate(component: boolean, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot | undefined): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    if(localStorage.getItem('complete')==='true') return true
-    else {
+    let statusForm=JSON.parse(localStorage.getItem('complete')||'{}')
+    if(statusForm.reg) return true
+    else if(statusForm.campos>0){
       return Swal.fire({
-        title: 'Seguro que desea salir sin completar el formulario',
+        title: `Seguro que desea salir sin completar ${statusForm.campos} campos del formulario`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si',
+        heightAuto:false
+      }).then((result) => {
+        if (result.isConfirmed){
+          return true
+        }else return false
+      }    
+      )
+    }else{
+      return Swal.fire({
+        title: `Seguro que desea salir guardar registrar el nuevo usuario`,
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
--- src/app/page/form/form.component.ts
+++ src/app/page/form/form.component.ts
@@ -1,3 +1,4 @@
+import  Swal  from 'sweetalert2';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
@@ -18,15 +19,44 @@ export class FormComponent implements OnInit {
       listaUser.push({name:this.form.value.user, pass:this.form.value.pass})/* crea un nuevo usuario */
       localStorage.setItem('user',JSON.stringify(listaUser))/* sobrescribe el local storage con el nuevo user*/
       this.usuario=this.form.value /* almacena el nuevo user añadido*/
-      this.form.reset()
-      localStorage.setItem('complete','true')/* valor para que su guard no se ejecute si se registro un user */
+      Swal.fire({
+        icon:'success',
+        title:'Registro exitoso',
+        heightAuto:false
+      })
+    }else{
+      Swal.fire({
+        icon:'error',
+        title:'Registro fallido',
+        heightAuto:false
+      })
     }
+    this.cambio()
+    this.form.reset()
 
   }
   cambio(){
-    localStorage.setItem('complete','false')/* funcion para que cuando el usuario modifique un input el guard se active si sale de la pagina */
+    
+    let camposFalta=0
+    if(this.form.get('name')?.value=='') camposFalta++
+    if(this.form.get('user')?.value=='') camposFalta++
+    if(this.form.get('email')?.value=='') camposFalta++
+    if(this.form.get('pass')?.value=='') camposFalta++
+    if(this.form.get('pass2')?.value=='') camposFalta++
+
+    let bd=JSON.parse(localStorage.getItem('user')||'{}')
+    for(let i of bd){
+      if(this.form.value.name==i.name && this.form.value.pass==i.pass){
+        localStorage.setItem('complete',`{'campos':${camposFalta},'reg':true}`)
+      }
+      else{
+        localStorage.setItem('complete',`{'campos':${camposFalta},'reg':false}`)
+      }
+    }
+    
   }
   ngOnInit(): void {
+    
     this.form=this.fb.group({
       name:[
         '',
@@ -62,6 +92,7 @@ export class FormComponent implements OnInit {
       ]
       
     })
+    this.cambio()
   }
 
 }
